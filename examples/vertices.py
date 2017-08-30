666f403563f5a8e345c9d4c97263e588d15e4f97
William Doran <wfdoran4@yahoo.com>
2017-08-30 13:08:45 -0700

    Added a try/except in linear alg to deal with singular matrices.

--- examples/vertices.py
+++ examples/vertices.py
@@ -2,8 +2,13 @@ import numpy as np
 import itertools
 
 # example from wikipedia
-a = [[-1,1],[3,2],[2,3],[-1,0],[0,-1]]
-b = [1,12,12,0,0]
+#a = [[-1,1],[3,2],[2,3],[-1,0],[0,-1]]
+#b = [1,12,12,0,0]
+
+# example from http://web.mit.edu/16.410/www/lectures_fall04/L18-19-IP-BB.pdf
+a = [[6,3,5,2], [0,0,1,1], [-1,0,1,0], [0,-1,0,1], [1,0,0,0], [0,1,0,0], 
+     [0,0,1,0], [0,0,0,1], [-1,0,0,0], [0,-1,0,0], [0,0,-1,0], [0,0,0,-1]]
+b = [10, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0]
 
 n = len(a)
 k = len(a[0])
@@ -16,17 +21,19 @@ for s in itertools.combinations(range(n),k):
         bb.append(b[x])
     aaa = np.array(aa)
     bbb = np.array(bb)
-    x = np.linalg.solve(aaa,bbb)
+    try:
+        x = np.linalg.solve(aaa,bbb)
     
-    good = True
-    for i in range(n):
-        t = 0
-        for j in range(k):
-            t += x[j] * a[i][j]
-        if t > b[i]:
-            good = False
-    if good:
-        print(s,aa,bb,x)
-    
+        good = True
+        for i in range(n):
+            t = 0
+            for j in range(k):
+                t += x[j] * a[i][j]
+            if t > b[i]:
+                good = False
+        if good:
+            print(s,aa,bb,x)
+    except:
+        pass
     
     
