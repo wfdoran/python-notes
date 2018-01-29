c8814feb13ca585719b0bac79f795b1fba9f821e
William Doran <wfdoran4@yahoo.com>
2018-01-29 13:10:35 -0800

    Simple multi-process example.

---
+++ parallel/parallel_ex1.py
@@ -0,0 +1,21 @@
+import multiprocessing as mp
+import time
+
+def hello(proc_id, channel):
+    print("start: ", proc_id)
+    channel.put(str(proc_id))
+    time.sleep(1)
+    print("end: ", proc_id)
+
+num_procs = 4
+channel = mp.Queue()
+procs = [mp.Process(target=hello, args=(proc_id, channel)) for proc_id in range(num_procs)]
+
+for p in procs:
+    p.start()
+
+for p in procs:
+    p.join()
+
+results = [channel.get() for p in procs]
+print(results)
